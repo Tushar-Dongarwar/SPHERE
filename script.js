8dbc913a64ccf114b764ffc7cfb426b84abf8e02
Tushar Anil Dongarwar <103037404+Tushar-Dongarwar@users.noreply.github.com>
2022-04-15 15:17:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const textureLoader = new THREE.TextureLoader()
 
-const normalTexture = textureLoader.load('./Earth.jpg')
+const normalTexture = textureLoader.load('./Earth.jpeg')
 const normalTextureSUN = textureLoader.load('./Sun.jpeg')
 
 
