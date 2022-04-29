123e52fc8d49b1804fdaea39ddb325720186715a
Tushar Anil Dongarwar <103037404+Tushar-Dongarwar@users.noreply.github.com>
2022-04-29 11:04:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -152,9 +152,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 10
+camera.position.x = 33
+camera.position.y = 8
+camera.position.z = -0.4
 scene.add(camera)
 
 // Controls
